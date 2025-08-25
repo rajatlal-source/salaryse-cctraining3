5c89d76e0e3ecd333b7226228fa215e74a726997
rajatlal-source <rajat.lal@salaryse.com>
2025-08-25 14:43:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ss: {
+          purple: '#3D41FA',
+          yellow: '#EEFF41',
+          ink: '#0B0B0B'
+        }
+      },
+      borderRadius: {
+        xl2: '1.25rem'
+      }
+    }
+  },
+  plugins: []
+}
